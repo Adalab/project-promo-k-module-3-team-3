c9f3e4aee5153bb6d558f02e675912caca979345
Claudia Palazon <claupanon@gmail.com>
2020-11-03 13:50:28 +0100

    Partials react header and footer

--- my-react-project/src/components/App.js
+++ my-react-project/src/components/App.js
@@ -1,22 +1,44 @@
 import React from "react";
+import logoHeader from "../images/Catrinas-7.png";
+import logoFooterCatrinas from "../images/catrina.png";
+import logoFooterAdalab from "../images/logo-adalab.png";
 import "../stylesheets/App.scss";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>Holis!</p>
+      <>
+        <header className="header" role="banner">
+          <a href="./index.html" className="header__link">
+            <img
+              className="header__link__logo"
+              src={logoHeader}
+              alt="Awesome profile cards Logo"
+              title="Página principal"
+            />
+          </a>
         </header>
-        <main>
-          <div className="principal">
-            <form className="form">
-              <label htmlFor="name">Nombre:</label>
-              <input type="text" id="name" />
-            </form>
+        <footer class="footer" role="contentinfo">
+          <div class="footer__box">
+            <img
+              class="footer__icon"
+              src={logoFooterCatrinas}
+              alt="Catrinas logo"
+              title="Logotipo Catrinas"
+            />
+            <small class="footer__copy txt-fsec txt-lighter txt-sm">
+              Catrinas profile-cards @2020
+            </small>
           </div>
-        </main>
-      </div>
+          <a href="https://adalab.es/" target="_blank" class="footer__logo">
+            <img
+              class="footer__logo__img"
+              src={logoFooterAdalab}
+              title="Adalab"
+            />
+          </a>
+        </footer>
+      </>
     );
   }
 }
